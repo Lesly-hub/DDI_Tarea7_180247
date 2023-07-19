487722e9b45bbf3c9333eb7f909c2bd231740855
Lesly_hub <lesly.yareth150201@gmail.com>
2023-07-18 23:54:01 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DDI_TAREA7_180247
+
+Implementacion de la Hora en dispositivos Wearebles
+Nombre:Lesly Yareth Hernandez Bonilla
+Matricula:180247
+Materia: Desarrollo para Dispositivos Inteligentes
+Docente: M.T.I Marco A. Ramirez Hernandez
---
+++ app/src/main/java/mx/edu/utxj/ti/idgs/ddi/tarea7_180247/presentation/MainActivity.kt
@@ -0,0 +1,67 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package mx.edu.utxj.ti.idgs.ddi.tarea7_200687.presentation
+
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import mx.edu.utxj.ti.idgs.ddi.tarea7_200687.R
+import mx.edu.utxj.ti.idgs.ddi.tarea7_200687.presentation.theme.tarea7_200687Theme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+    private var mHandler: Handler? = null
+    private var mRunnable: Runnable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mHandler = Handler()
+        mRunnable = Runnable {
+            updateTime()
+            mRunnable?.let { mHandler?.postDelayed(it, 1000) }
+        }
+
+    }
+
+    private fun updateTime() {
+        val timeTextView = findViewById<TextView>(R.id.HoraParaVisualizar)
+
+        val currentTime = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+
+        timeTextView.text = currentTime
+    }
+    override fun onResume() {
+        super.onResume()
+        mRunnable?.let { mHandler?.post(it) }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mRunnable?.let { mHandler?.removeCallbacks(it) }
+    }
+
+
+}
